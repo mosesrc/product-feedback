5945ec35f1541a92e3417687209331ef3bdc663d
ryan.moses <ryan.c.moses.ctr@mail.mil>
2023-09-12 22:49:58 -0400

    Initial commit for project 	- folder organization complete

---
+++ src/components/UI/TagButton/TagButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./TagButton.module.css";
+
+const TagButton = (props) => {
+    return <button>State Value</button>;
+};
+
+export default TagButton;
---
+++ src/components/UI/UIRoot/UIRoot.js
@@ -0,0 +1,14 @@
+import classes from "./UIRoot.module.css";
+import TagButton from "../TagButton/TagButton";
+
+const UIRoot = () => {
+    return (
+        <>
+            <div>
+                <h1>In UI Root</h1>
+            </div>
+        </>
+    );
+};
+
+export default UIRoot;
